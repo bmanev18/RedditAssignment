3a3e54cc21ac5cc5399599228817418e1bd700b0
radoslavKiryazov <radokiryazov11230@gmail.com>
2023-03-31 16:44:15 +0200

    Step4: webApi
    - created and tested initial webAPI:
    - added inter-project dependencies
    - fixed CORS
    - edited User.cs
    - added authentication with info about JWT in WebAPI/Program.cs
    - added information for the JWT to generate
    - created Post.cs

---
+++ Shared/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared.Models;
+
+public class Post
+{
+ public int Id;
+ public User Owner;
+ public String Title;
+ public String Body;
+
+
+ public Post(User owner, String title, String body)
+ {
+  Owner = owner;
+  Title = title;
+  Body = body;
+ }
+}
