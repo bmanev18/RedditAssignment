2001a37fb81405c0e435a272d1a2dd98a5fb63ee
Bozhidar Manev <bojomanev@gmail.com>
2023-04-02 18:05:57 +0200

    Login is working
    - login added in BlazorWasm
    - logout added
    - register method exists, page needed to be completed

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasm;
+using BlazorWasm.Auth;
+using BlazorWasm.Services;
+using BlazorWasm.Services.Http;
+using Microsoft.AspNetCore.Components.Authorization;
+using Shared.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IAuthService, JwtAuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+
+AuthorizationPolicies.AddPolicies(builder.Services);
 
 await builder.Build().RunAsync();
